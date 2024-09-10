490f2b2a415c9fdcc97e90a2442b1a1dfe02fc55
InvestUz <invest.abdusattorov@gmail.com>
2024-09-10 18:13:02 +0500

    upgrading create page with adding loading component

--- pages/product/create.vue
+++ pages/product/create.vue
@@ -8,25 +8,28 @@
                 </h4>
             </div>
             <div class="card-body">
-                <form>
+                <div v-if="isLoading">
+                  <Loading :title="isLoadingTitle"/>
+                </div>
+                <form @submit.prevent="saveProduct">
                     <div class="mb-3">
                         <label for="name">Name</label>
-                        <input type="text" class="form-control" required>
+                        <input type="text" v-model="product.name" class="form-control">
                     </div>
                     <div class="mb-3">
                         <label for="color">Color</label>
-                        <input type="text" class="form-control" required>
+                        <input type="text" v-model="product.color" class="form-control">
                     </div>
                     <div class="mb-3">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" required>
+                        <input type="email" v-model="product.email" class="form-control">
                     </div>
                     <div class="mb-3">
                         <label for="phone">Phone</label>
-                        <input type="text" class="form-control" required>
+                        <input type="text" v-model="product.phone" class="form-control">
                     </div>
                     <div class="mb-3">
-                        <button class="btn btn-primary">Save</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
                     </div>
                 </form>
             </div>
@@ -35,8 +38,30 @@
 </template>
 
 <script>
-export default{
-    name: 'productCreate'
+export default {
+    name: 'productCreate',
+    data() {
+        return {
+            product: {
+                name: '',
+                color: '',
+                email: '',
+                phone: '',
+            },
+            isLoading: false,
+            isLoadingTitle: 'Loading',
+
+        }
+    },
+    methods: {
+        saveProduct() {
+            this.isLoading = true;
+            this.isLoadingTitle = 'Saving'
+
+
+            // console.log("am here")
+        }
+    }
 }
 
 </script>
